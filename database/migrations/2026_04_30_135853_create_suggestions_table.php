62879345a416eb68bb28e8c04106cf30f52aee04
AdityaGG81 <adityaghorpade81@gmail.com>
2026-05-08 12:13:07 +0530

    fix: added table check to suggestions migration

--- database/migrations/2026_04_30_135853_create_suggestions_table.php
+++ database/migrations/2026_04_30_135853_create_suggestions_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('suggestions', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->nullable();
-            $table->string('role');
-            $table->text('message');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('suggestions')) {
+            Schema::create('suggestions', function (Blueprint $table) {
+                $table->id();
+                $table->string('name')->nullable();
+                $table->string('email')->nullable();
+                $table->string('role');
+                $table->text('message');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
